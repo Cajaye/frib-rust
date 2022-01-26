8f766db20ce4adca32dd87936c0b0ff9fa9c44cd
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-26 08:42:21 -0500

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    let fib = fib_num(15, 1, 1);
+    println!("{}", fib);
+}
+
+fn fib_num(nth: usize, mut a: usize, mut b: usize) -> usize {
+    loop {
+        if b > nth {
+            break b;
+        }
+        let c = a + b;
+        a = b;
+        b = c;
+    }
 }
